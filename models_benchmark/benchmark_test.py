583967156629c54aaf48cac9e82f07f9b753ce97
alessandro <alessandronico22@gmail.com>
2020-11-06 20:43:09 +0100

    intermidiate modification

--- models_benchmark/benchmark_test.py
+++ models_benchmark/benchmark_test.py
@@ -1,8 +1,2 @@
-import torch
-from torch.utils.data import Subset, DataLoader
-from torchvision import transforms
-import numpy as np
 
-# project classes --------------------------------------------------------------
-from dataset.ilcifar100 import ilCIFAR100
-from baselines.resnet import resnet32
+
